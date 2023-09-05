1e2497f45536e7aebed4132c73340b0200eb5ec1
Nick Frasser <nfrasser@gmail.com>
2023-09-04 20:48:51 -0400

    Install netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config"
+import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		assets: true
-	},
 	integrations: [],
 	vite: {
 		ssr: {
 			noExternal: ["milligram"]
 		}
-	}
+	},
+	output: "server",
+	adapter: netlify()
 })
