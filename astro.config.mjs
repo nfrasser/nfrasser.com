7f28cd2aa8e90f1800cad441886348f81c3b098d
Nick Frasser <1693461+nfrasser@users.noreply.github.com>
2025-03-23 16:15:17 -0400

    Upgrade deps and add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config"
 
+import cloudflare from "@astrojs/cloudflare"
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [],
+
 	vite: {
 		ssr: {
 			noExternal: ["milligram"]
 		}
-	}
+	},
+
+	adapter: cloudflare()
 })
